31427d348f38ac259c22fe6b7fb0c544a7c20b2f
Arthur <64021939+ArthurBajt@users.noreply.github.com>
2022-03-14 18:06:39 +0100

    User Repository

--- src/main/java/com/example/ideabox/IdeaBoxApplication.java
+++ src/main/java/com/example/ideabox/IdeaBoxApplication.java
@@ -1,5 +1,8 @@
 package com.example.ideabox;
 
+import com.example.ideabox.User.User;
+import com.example.ideabox.User.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +12,6 @@ public class IdeaBoxApplication {
     public static void main(String[] args) {
         SpringApplication.run(IdeaBoxApplication.class, args);
     }
+
 
 }
--- src/main/java/com/example/ideabox/User/User.java
+++ src/main/java/com/example/ideabox/User/User.java
@@ -22,6 +22,77 @@ public class User {
     @Column
     private String firstName;
 
-    @Column
-    private String nom;
+
+    public User() {
+    }
+
+
+    public User(String email, String pseudo, String lastName, String firstName) {
+        this.email = email;
+        this.pseudo = pseudo;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+
+    public User(long id, String email, String pseudo, String lastName, String firstName) {
+        this.id = id;
+        this.email = email;
+        this.pseudo = pseudo;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", pseudo='" + pseudo + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                '}';
+    }
 }
