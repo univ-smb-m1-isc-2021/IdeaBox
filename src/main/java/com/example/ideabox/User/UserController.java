d450d42218b9c4dd31e3d2f6bbdf27f8151c3c9f
Léo <58537513+leolaf@users.noreply.github.com>
2022-04-08 17:05:23 +0200

    users dans le home

--- src/main/java/com/example/ideabox/User/UserController.java
+++ src/main/java/com/example/ideabox/User/UserController.java
@@ -2,7 +2,10 @@ package com.example.ideabox.User;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,12 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
 
-    @GetMapping("/logIn")
-    public String showLogIn(){
+    @GetMapping("/login")
+    public String showLogIn(Model model){
+        model.addAttribute("user",new User());
+        model.addAttribute("password", new String());
         return "login";
     }
 
-    @GetMapping("/signUp")
+    @PostMapping("/login")
+    public String submitLogIn(@ModelAttribute User user, @ModelAttribute String password, Model model){
+        user.setHashpassword(password);
+        model.addAttribute("user",user);
+        //Verif dans la bdd
+        return "home";
+    }
+
+    @GetMapping("/signup")
     public String showSignUp(){
         return "signup";
     }
