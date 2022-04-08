d1566982acec5d706b1207470877e87981c88714
Arthur <64021939+ArthurBajt@users.noreply.github.com>
2022-04-08 18:16:41 +0200

    fix security

--- src/main/java/com/example/ideabox/security/SecurityConfig.java
+++ src/main/java/com/example/ideabox/security/SecurityConfig.java
@@ -3,12 +3,19 @@ package com.example.ideabox.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.httpBasic().disable();    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
